491571453b8ae190102ba96c65a5ba34eb5fed17
Sohail Memon <sohailmemon10@gmail.com>
2022-08-01 02:25:52 +0530

    did some changes

--- controllers/petController.js
+++ controllers/petController.js
@@ -1,25 +1,34 @@
 const xlsx = require("xlsx");
 const path = require("path");
+const fs = require("fs");
 const Pet = require("../models/Pet");
 
 module.exports.addPet = (req, res) => {
   let pets = [];
+  let error = false;
   // Working with excel file
-  const workbook = xlsx.readFile(
-    path.join(__dirname, "../petData/uploadedFile.xlsx")
-  );
-  const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-  const worksheetLength = (Object.keys(worksheet).length - 2) / 4;
-  for (let i = 2; i <= worksheetLength; i++) {
-    const name = worksheet[`A${i}`].v;
-    const type = worksheet[`B${i}`].v;
-    const breed = worksheet[`C${i}`].v;
-    const age = worksheet[`D${i}`].v;
-    pets.push(new Pet({ name, type, breed, age }));
+  try {
+    const workbook = xlsx.readFile(
+      path.join(__dirname, "../petData/uploadedFile.xlsx")
+    );
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    const worksheetLength = (Object.keys(worksheet).length - 2) / 4;
+    for (let i = 2; i <= worksheetLength; i++) {
+      const name = worksheet[`A${i}`].v;
+      const type = worksheet[`B${i}`].v;
+      const breed = worksheet[`C${i}`].v;
+      const age = worksheet[`D${i}`].v;
+      pets.push(new Pet({ name, type, breed, age }));
+    }
+  } catch {
+    error = true;
   }
 
+  // Deleting UploadedFile
+  try {
+    fs.unlinkSync(path.join(__dirname, "../petData/uploadedFile.xlsx"));
+  } catch (err) {}
   // Saving pets to db
-  let error = false;
   pets.forEach((pet) => {
     pet
       .save()
@@ -38,7 +47,10 @@ module.exports.getAllPets = (req, res) => {
 };
 
 module.exports.getPet = (req, res) => {
-    
+  Pet.findById(req.params.pet_id, (err, pet) => {
+    if (pet) res.json({ pet, message: "Get pet" });
+    if (err) res.json({ message: "No such pet" });
+  });
 };
 
 module.exports.updatePet = (req, res) => {};
