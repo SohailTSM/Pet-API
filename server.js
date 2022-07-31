1f748f7d4752771b471c89d54219cafccac5de2e
Sohail Memon <sohailmemon10@gmail.com>
2022-07-31 12:40:28 +0530

    connectng to db

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 require('dotenv/config');
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+mongoose.connect(process.env.DB_CONNECTION, { dbName: 'Pets' }, () => {
+  app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000');
+  });
 });
